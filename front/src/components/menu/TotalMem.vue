e90ed4993520914571b223b1a2ab9c57c81d67d1
lsh <jetaime12121@gmail.com>
2021-11-24 18:49:25 +0900

    v-for, v-if 사용법

---
+++ front/src/components/menu/TotalMem.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="member in test" v-bind:key="member.index">
+                이름: {{member.name}}
+                나이: {{member.age}}
+            </li>
+        </ul>
+        <div v-if="top">
+            보이냐
+        </div>
+        <div v-show="top">
+            보이냐
+        </div>
+        <button v-on:click="show">보였다가 안보였다가</button>
+        {{this.$store.state.name}}
+    </div>
+</template>
+
+<script>
+export default {
+    
+    data() {
+        return {
+            top: false,
+            test : this.$store.state.members 
+        }
+    },
+    methods: {
+        show() {
+            this.top =! this.top;
+            //true 
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
--- front/src/views/IntroduceView.vue
+++ front/src/views/IntroduceView.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="wrapAll">
         <introduce-form></introduce-form>
+        <total-mem></total-mem>
     </div>
 </template>
 
 <script>
 import IntroduceForm from '@/components/menu/IntroduceForm.vue'
+import TotalMem from '../components/menu/TotalMem.vue'
 export default {
-    components: { IntroduceForm },
+    components: { IntroduceForm, TotalMem },
 
 }
 </script>
